94dba9fb3ac7f9872c495e6264d89a350e8926bf
shawnt96 <shawn_t1@live.com>
2016-10-14 18:24:14 -0500

    exercise2

--- maze.js
+++ maze.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+/*window.onload = function(){
 
 
 document.getElementById("boundary1").onmouseover = myfunction;
@@ -7,7 +7,18 @@ document.getElementById("boundary1").onmouseover = myfunction;
 
 function myfunction(){
 	document.getElementById("boundary1").className="boundary youlose";
+}*/
+
+window.onload = function(){
+	var numBoundaries1 = document.querySelectorAll("#maze .boundary");
+	for(var num = 0; num < numBoundaries1.length; num++ )
+		numBoundaries1[num].onmouseover = myfunction;
 }
 
+function myfunction(){
+	var numBoundaries2 = document.querySelectorAll("#maze .boundary");
+	for(var num = 0; num < numBoundaries2.length; num++)
+		numBoundaries2[num].className ="boundary youlose";
+}
 
 
