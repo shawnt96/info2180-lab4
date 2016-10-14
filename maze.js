2ed928cf99adcf63f2ec118e4279836b5cded46a
shawnt96 <shawn_t1@live.com>
2016-10-14 13:13:47 -0500

    first

---
+++ maze.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+
+
+document.getElementById("boundary1").onmouseover = myfunction;
+
+}
+
+function myfunction(){
+	document.getElementById("boundary1").className="boundary youlose";
+}
+
+
+
