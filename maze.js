41b8bc3f0cd6fadefe13bbbe1bbba7d2e41a0fcd
shawnt96 <shawn_t1@live.com>
2016-10-14 23:51:33 -0500

    changes(exercise3)

--- maze.js
+++ maze.js
@@ -1,38 +1,5 @@
-/*window.onload = function(){
 
-
-document.getElementById("boundary1").onmouseover = myfunction;
-
-}
-
-function myfunction(){
-	document.getElementById("boundary1").className="boundary youlose";
-}*/
-
-<<<<<<< HEAD
-window.onload = function(){
-=======
-/*window.onload = function(){
->>>>>>> refs/remotes/origin/master
-	var numBoundaries1 = document.querySelectorAll("#maze .boundary");
-	for(var num = 0; num < numBoundaries1.length; num++ )
-		numBoundaries1[num].onmouseover = myfunction;
-}
-
-function myfunction(){
-<<<<<<< HEAD
-	var numBoundaries2 = document.querySelectorAll("#maze .boundary");
-	for(var num = 0; num < numBoundaries2.length; num++)
-		numBoundaries2[num].className ="boundary youlose";
-=======
-	var option = true;
-	var numBoundaries2 = document.querySelectorAll("#maze .boundary");
-	for(var num = 0; num < numBoundaries2.length; num++){		
-		numBoundaries2[num].className ="boundary youlose";
-		
-	}
-}*/
-var option =false;
+var option = false;
 var numBoundaries1;
 window.onload = function(){
 	document.getElementById("end").onmouseover = myFunction2;
@@ -41,7 +8,6 @@ window.onload = function(){
 	for(var num = 0; num < numBoundaries1.length; num++ )
 		numBoundaries1[num].onmouseover = myFunction;
 	
->>>>>>> refs/remotes/origin/master
 }
 
 function myFunction(){
@@ -50,13 +16,11 @@ function myFunction(){
 		numBoundaries1[num].className ="boundary youlose";
 		option = true;
 	}
-	
 		
 }
 	
 function myFunction2(){
 	if(!option)
 		alert("You Win!");
-	
 }
 
